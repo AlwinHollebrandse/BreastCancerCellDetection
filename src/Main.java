364931ce45cfa1ca65af4dca7da2817fea7ab7ce
AlwinHollebrandse <31869015+AlwinHollebrandse@users.noreply.github.com>
2020-02-06 18:41:03 -0500

    fixed filter looping bug

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,14 @@ public class Main {
 
     public static void main(String args[])throws IOException {
 
-        BufferedImage avgImage = null;
+        BufferedImage workingImage = null;
         try {
             final File input_file = new File("black-and-white-tips.jpg"); //image file path
 
             final BufferedImage originalImage = ImageIO.read(input_file);
-            avgImage = avgFilter(originalImage, 3, 3, new double[]{.5, .5, .5, .5, 1, .5, .5, .5, .5}, 1); // TODO ask why scalar, and how to normalize weights array + pixel
+            System.out.println("original x: " + originalImage.getWidth() + ", y: " + originalImage.getHeight()); // 350x211
+            workingImage = convertToGrayScale(originalImage);
+            workingImage = avgFilter(workingImage, 5, 5, null, 1);//new double[]{.5, .5, .5, .5, 1, .5, .5, .5, .5}, 1); // TODO ask why scalar, and how to normalize weights array + pixel
 
             System.out.println("Reading complete.");
         }
@@ -31,7 +33,7 @@ public class Main {
              File output_file = new File("avg-black-and-white-tips.jpg");
 
              // Writing to file taking type and path as
-             ImageIO.write(avgImage, "jpg", output_file);
+             ImageIO.write(workingImage, "jpg", output_file);
 
              System.out.println("Writing complete.");
          }
@@ -74,17 +76,21 @@ public class Main {
 
         // CROP BORDER VERSION
         BufferedImage avgImage = new BufferedImage(originalImage.getWidth() - filterWidth, originalImage.getHeight() - filterHeight, originalImage.getType());
-        int imageTotal = (originalImage.getWidth() - filterWidth) * (originalImage.getHeight() - filterHeight);
+
+        // endingX and Y need to be avgImage.getWidth() and y version - 1.
+        int endingXCoordinate = avgImage.getWidth() - 1;
+        int endingYCoordinate = avgImage.getHeight() - 1;
+
+        int imageTotal = endingXCoordinate * endingYCoordinate;
         ProgressBar bar = new ProgressBar("avgFilter", imageTotal);
 
-        int endingXCoordinate = originalImage.getWidth() - filterWidth;
-        int endingYCoordinate = originalImage.getHeight() - filterHeight;
-
-        for (int x = filterWidth/2; x <= endingXCoordinate; x++) {
-            for (int y = filterHeight/2; y <= endingYCoordinate; y++) {
-                ArrayList<Integer> neighborRGBValueArray = getNeighborRGBs(originalImage, x, y, filterHeight, filterWidth);
-                int avgRGBValue = calcAvgRGB(neighborRGBValueArray, weights);
-                avgImage.setRGB(x - filterWidth/2, y - filterHeight/2, avgRGBValue);
+        // loop through new cropped version size
+        for (int x = 0; x < endingXCoordinate; x++) {
+            for (int y = 0; y < endingYCoordinate; y++) {
+                ArrayList<Integer> neighborRGBValueArray = getNeighborGray(originalImage, (x + filterWidth/2), (y + filterHeight/2), filterHeight, filterWidth);
+                int avgRGBValue = calcAvgGray(neighborRGBValueArray, weights);
+                // TODO multiple each pixel by the scalar.
+                avgImage.setRGB(x, y, avgRGBValue);
                 bar.next();
             }
         }
@@ -103,7 +109,6 @@ public class Main {
 //            }
 //        }
 
-        // TODO multiple each pixel by the scalar.
         return avgImage;
     }
 
@@ -112,14 +117,35 @@ public class Main {
         ArrayList<Integer> neighborRGBValueArray = new ArrayList<>();
 
         // get the starting and ending valid coordinate values
-        int startingX = (originalX - filterWidth/2 >= 0) ? (originalX - filterWidth/2) : 0;
-        int startingY = (originalY - filterHeight/2 >= 0) ? (originalY - filterHeight/2) : 0;
-        int endingX = (originalX + filterWidth/2 + 1 <= originalImage.getWidth()) ? (originalX + filterWidth/2 + 1) : originalImage.getWidth();
-        int endingY = (originalY + filterHeight/2 + 1 <= originalImage.getHeight()) ? (originalY + filterHeight/2 + 1) : originalImage.getHeight();
+        int startingX = originalX - filterWidth/2;
+        int startingY = originalY - filterHeight/2;
+        int endingX = originalX + filterWidth/2;
+        int endingY = originalY + filterHeight/2;
 
         // get the pixels within the designated borders
-        for (int x = startingX; x < endingX; x++) {
-            for (int y = startingY; y < endingY; y++) {
+        for (int x = startingX; x <= endingX; x++) {
+            for (int y = startingY; y <= endingY; y++) {
+                neighborRGBValueArray.add(originalImage.getRGB(x, y));
+            }
+        }
+
+        return neighborRGBValueArray;
+    }
+
+    // NOTE this only supports odd rectangles with a center pixel. (ex: 3x3, 5x3, etc not 4x4) // TODO delte this method. its the same as the RGB one
+    public static ArrayList<Integer> getNeighborGray (BufferedImage originalImage, int originalX, int originalY, int filterHeight, int filterWidth) {
+        ArrayList<Integer> neighborRGBValueArray = new ArrayList<>();
+
+        // get the starting and ending valid coordinate values
+        int startingX = originalX - filterWidth/2;
+        int startingY = originalY - filterHeight/2;
+        int endingX = originalX + filterWidth/2;
+        int endingY = originalY + filterHeight/2;
+
+        // get the pixels within the designated borders
+        for (int x = startingX; x <= endingX; x++) {
+            for (int y = startingY; y <= endingY; y++) {
+//                System.out.println("avg x: " + x +", y: " + y);
                 neighborRGBValueArray.add(originalImage.getRGB(x, y));
             }
         }
@@ -147,6 +173,38 @@ public class Main {
         return neighborRGBValueArray;
     }
 
+
+    public static int calcAvgGray (ArrayList<Integer> list, double[] weights) throws NullPointerException {
+        if (list.size() != weights.length) {
+            throw new NullPointerException("weights array was not the size of the filter");
+        }
+
+        int result = 0;
+
+        for (int i = 0; i < list.size(); i++) { // img.getRGB(x, y)& 0xFF;
+            result += (list.get(i) & 0xFF) * weights[i]; // element * weight of pixel
+        }
+        System.out.println();
+//        System.out.println(list.toString());
+//        System.out.println("avg gray pixel value: " + result);
+        result /= list.size();
+//        System.out.println("avg gray pixel value: " + result);
+        System.out.println("center gray pixel value: " + (list.get(list.size()/2)  & 0xFF));
+
+        int centerPixelValue= list.get(list.size()/2); // gets everything about the center pixel, including intensity and such
+        result = (centerPixelValue & 0xFFFFFF00) | result; // keeps all of the intensity and suck values and only changes the gray scale value
+        // bug has something to do with the other bits of the gray, the leading ones... I think its because i
+//        System.out.println("avg gray pixel value: " + (result));
+        System.out.println("avg gray pixel value: " + (result  & 0xFF));
+
+
+
+        return result;
+    }
+
+
+    // TODO could add color filter here (RGB intensity)
+    // does the above but for red, green, and blue at once
     public static int calcAvgRGB (ArrayList<Integer> list, double[] weights) throws NullPointerException {
         if (list.size() != weights.length) {
             throw new NullPointerException("weights array was not the size of the filter");
@@ -173,5 +231,56 @@ public class Main {
         result = (result | blueAvg); // after this line, result is 0x00(red)(green)(blue)
 
         return result;
+    }
+
+
+
+
+
+
+
+
+
+
+
+    public static BufferedImage convertToGrayScale(BufferedImage originalImage) {
+        // NOTE if the assignment allowed for built in functionality of this sort, the simplest code would be: (this would replace the rest of this method)
+//        ImageFilter filter = new GrayFilter(true, 50);
+//        ImageProducer producer = new FilteredImageSource(colorImage.getSource(), filter);
+        if (originalImage.getType() == BufferedImage.TYPE_BYTE_GRAY) {
+            return originalImage;
+        }
+
+        if (originalImage.getType() == BufferedImage.TYPE_INT_RGB) {
+            BufferedImage grayScaleImage = new BufferedImage(originalImage.getWidth(), originalImage.getHeight(), originalImage.getType());
+
+            for (int x = 0; x < originalImage.getWidth(); x++) {
+                for (int y = 0; y < originalImage.getHeight(); y++) {
+
+                    // from https://stackoverflow.com/questions/9131678/convert-a-rgb-image-to-grayscale-image-reducing-the-memory-in-java
+                    int rgb = originalImage.getRGB(x, y);
+                    int r = (rgb >> 16) & 0xFF;
+                    int g = (rgb >> 8) & 0xFF;
+                    int b = (rgb & 0xFF);
+
+                    // Normalize and gamma correct:
+                    double rr = Math.pow(r / 255.0, 2.2);
+                    double gg = Math.pow(g / 255.0, 2.2);
+                    double bb = Math.pow(b / 255.0, 2.2);
+
+                    // Calculate luminance:
+                    double lum = 0.2126 * rr + 0.7152 * gg + 0.0722 * bb;
+
+                    // Gamma compand and rescale to byte range:
+                    int grayLevel = (int) (255.0 * Math.pow(lum, 1.0 / 2.2));
+                    int gray = (grayLevel << 16) + (grayLevel << 8) + grayLevel;
+                    grayScaleImage.setRGB(x, y, gray);
+                }
+            }
+            return grayScaleImage;
+        }
+
+        System.out.println("The system cannot handel images of this type");
+        return null;
     }
 }
